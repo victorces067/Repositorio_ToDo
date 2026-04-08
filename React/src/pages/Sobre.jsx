75b97b206140931aac1b45005f20c7b1501704b4
victor.silva <victor.ces067@gmail.com>
2026-04-08 10:37:46 -0300

    Delete React directory

--- React/src/pages/Sobre.jsx
+++
@@ -1,8 +0,0 @@
-export default function Sobre() {
-  return (
-    <div>
-      <h1>Sobre</h1>
-      <p>Projeto criado para praticar React com rotas e componentes.</p>
-    </div>
-  );
-}
--- React/src/pages/Tarefas.jsx
+++
@@ -1,9 +0,0 @@
-import ToDo from "../components/ToDo";
-
-export default function Tarefas() {
-  return (
-    <div>
-      <ToDo />
-    </div>
-  );
-}
